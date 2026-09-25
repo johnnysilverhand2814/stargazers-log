87b6ac962a643753d5643a20ef3f6990d0099bf5
johnnysilverhand2814 <cwilsonbarrett@gmail.com>
2026-09-25 14:31:01 +0100

    Address review feedback
    Address review feedback

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-fetch("events.json")
-  .then((response) => response.json())
+ fetch("events.json")
+-  .then((response) => response.json())
++  .then((response) => {
++    if (!response.ok) {
++      throw new Error(`Unable to load starred repositories: ${response.status}`);
++    }
++    return response.json();
++  })
+   .then((events) => {
   .then((events) => {
     const list = document.querySelector("#starred");
     events.forEach((event) => {
